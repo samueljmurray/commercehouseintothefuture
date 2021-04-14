ff53212a55eff635c891f757974ee20278d74cec
Andrew Hughes-Onslow <andrew@kalpaitch.com>
2021-04-14 14:13:16 +0100

    Updated.

--- js/main.js
+++ js/main.js
@@ -4,58 +4,11 @@ const isotopeOptions = {
     itemSelector: '.grid-item',
     percentPosition: true,
     masonry: {
-        columnWidth: 260
+        columnWidth: 370
     }
 };
 let grid = document.querySelector('.flex-grid');
-const container = new Isotope(grid, isotopeOptions);
-
-// Filter based on changed search terms.
-document.querySelector('.quicksearch').addEventListener( 'change', function( event ) {
-    // Only work with inputs.
-    if ( !matchesSelector( event.target, 'input' ) ) {
-        return;
-    }
-    console.log('Filtering by search');
-    qsRegex = new RegExp( this.value, 'gi' );
-    container.arrange({
-        filter: function( index, itemElem ) {
-            var name = itemElem.querySelector('.name').innerText;
-            var title = itemElem.querySelector('.title').innerText;
-
-            return qsRegex ? name.match(qsRegex) || title.match(qsRegex) : true;
-        }
-    });
-});
-// Filter based on re-submitted search terms.
-document.querySelector('.filter-group').addEventListener( 'click', function( event ) {
-    // Only work with inputs.
-    if ( !matchesSelector( event.target, 'button.search-button' ) ) {
-        return;
-    }
-    console.log('Filtering by search');
-    let searchValue = document.getElementById('quicksearch').value;
-    qsRegex = new RegExp( searchValue, 'gi' );
-    container.arrange({
-        filter: function( index, itemElem ) {
-            var name = itemElem.querySelector('.name').innerText;
-            var title = itemElem.querySelector('.title').innerText;
-
-            return qsRegex ? name.match(qsRegex) || title.match(qsRegex) : true;
-        }
-    });
-});
-
-// Filter based on category.
-document.querySelector('.filter-group').addEventListener( 'click', function( event ) {
-    // Only work with explicitly labelled filter buttons.
-    if ( !matchesSelector( event.target, 'button.filter-button' ) ) {
-        return;
-    }
-    console.log('Filtering by group');
-    let filterValue = event.target.getAttribute('data-filter');
-    container.arrange({ filter: filterValue });
-});
+// const container = new Isotope(grid, isotopeOptions);
 
 // Play each audio story.
 function playStory(element, e) {
@@ -97,10 +50,35 @@ function playStory(element, e) {
     container.reloadItems()
 }
 
+// Play each audio story.
+function showStory(element, e) {
+    const feature = document.getElementById('feature');
+    feature.classList.remove("hidden");
+
+    // Hide all feature stories.
+    const stories = document.getElementsByClassName('story-item');
+    for(i=0; i<stories.length; i++) {
+        stories[i].classList.add("hidden");
+    }
+
+    // Show the right story.
+    const story = document.getElementById(element);
+    story.classList.remove("hidden");
+
+    // Scroll the feature box into view.
+    feature.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'nearest'
+    });
+
+
+}
+
 let elements = document.getElementsByClassName("story");
 for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', (e) => {
         let el = e.currentTarget.getAttribute('data-action')
-        playStory(el, e)
+        showStory(el, e)
     }, false);
 }
